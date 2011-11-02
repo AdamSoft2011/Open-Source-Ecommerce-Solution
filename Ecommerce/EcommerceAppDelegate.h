890d6f6f652082f17697a8ef76e17b23d9201b3b
猪小小 <Larry@Pengyu-Lans-iMac.local>
2011-11-02 19:41:14 +0000

    Initial Commit

---
+++ Ecommerce/EcommerceAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppDelegate.h
+//  Ecommerce
+//
+//  Created by 猪小小 on 02/11/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EcommerceAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
